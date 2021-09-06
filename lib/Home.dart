a3759c553d64b21b39d5caa35c1178d90562ff59
Lucca D'Amico Diniz <lucca.damico@hotmail.com>
2021-09-06 15:43:00 -0400

    Tela para testes de manipulação de dados

---
+++ lib/Home.dart
@@ -0,0 +1,72 @@
+import 'package:flutter/material.dart';
+
+class Home extends StatefulWidget {
+  const Home({Key? key}) : super(key: key);
+
+  @override
+  _HomeState createState() => _HomeState();
+}
+
+class _HomeState extends State<Home> {
+
+  TextEditingController _controllerCampo = TextEditingController();
+
+  _salvar(){
+
+  }
+
+  _recuperar(){
+
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Manipulação de dados"),
+      ),
+      body: Container(
+        padding: EdgeInsets.all(32),
+        child: Column(
+          children: [
+            Text(
+              "Nada salvo!",
+              style: TextStyle(
+                fontSize: 20
+              ),
+            ),
+            TextField(
+              keyboardType: TextInputType.text,
+              decoration: InputDecoration(
+                labelText: "Digite algo"
+              ),
+              controller: _controllerCampo,
+            ),
+            Row(
+              children: [
+                ElevatedButton(
+                    onPressed: _salvar,
+                    child: Text("Salvar"),
+                    style: ElevatedButton.styleFrom(
+                      primary: Colors.blue,
+                      onPrimary: Colors.white,
+                      textStyle: TextStyle(fontSize: 20)
+                    )
+                ),
+                ElevatedButton(
+                    onPressed: _recuperar,
+                    child: Text("Recuperar"),
+                    style: ElevatedButton.styleFrom(
+                        primary: Colors.blue,
+                        onPrimary: Colors.white,
+                        textStyle: TextStyle(fontSize: 20)
+                    )
+                )
+              ],
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
